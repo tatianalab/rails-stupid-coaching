819eddea9445a1cc812c27882150c3d94784cb21
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-09 12:31:16 -0500

    controller delivers coach answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-  end
+    @search = params[:question]
+    @answer =''
 
+    if @search.downcase == 'i am going to work right now!'
+      @answer = ''
+    elsif @search.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
 end
